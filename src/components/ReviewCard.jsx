ced292bd226fcb3e24ae9dce238336150a564066
kyrikoni <kyrikoni88@gmail.com>
2023-01-10 13:35:01 +0000

    adding singleReview pages based on parametric endpoints

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,1 +1,19 @@
-
+export const ReviewCard = ({ review }) => {
+  return (
+    <div className="ReviewCard" key={review.review_id}>
+      <p>Title: {review.title}</p>
+      <p>Category: {review.category}</p>
+      <p>Owner: {review.owner}</p>
+      <p>
+        <img
+          className="ReviewImages"
+          src={review.review_img_url}
+          alt={review.title}
+        />
+      </p>
+      <p>
+        <button>Full Review</button>
+      </p>
+    </div>
+  );
+};
